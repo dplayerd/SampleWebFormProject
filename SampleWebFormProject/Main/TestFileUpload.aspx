51915cb86865f75f4d148ddc537268e07df446df
Moudou <dplayerd@gmail.com>
2021-04-17 15:38:23 +0800

    檔案上傳 sample

---
+++ SampleWebFormProject/Main/TestFileUpload.aspx
@@ -0,0 +1,22 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="TestFileUpload.aspx.cs" Inherits="Main.TestFileUpload" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <img runat="server" id="img1" src="/FileDownload/child.jpg" />
+        <br />
+
+        <asp:FileUpload ID="FileUpload1" runat="server" />
+        <br />
+        <asp:Button ID="Button1" runat="server" Text="Upload" OnClick="Button1_Click" />
+        <br />
+        <asp:Label runat="server" ID="lbMsg" ForeColor="Red"></asp:Label>
+    </form>
+</body>
+</html>
---
+++ SampleWebFormProject/Main/TestFileUpload.aspx.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Main
+{
+    public partial class TestFileUpload : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            this.img1.Src = "/FileDownload/" + this.Session["NewFileName"];
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            if (!this.FileUpload1.HasFile)
+            {
+                this.lbMsg.Text = "File is required.";
+                return;
+            }
+
+            var uFile = this.FileUpload1.PostedFile;
+            var fileName = uFile.FileName;
+            string fileExt = System.IO.Path.GetExtension(fileName);
+
+            string path = Server.MapPath("~/FileDownload/");
+            string newFIleName = Guid.NewGuid().ToString() + fileExt;
+            string fullPath = System.IO.Path.Combine(path, newFIleName);
+
+            this.Session["NewFileName"] = newFIleName;
+
+            uFile.SaveAs(fullPath);
+            this.lbMsg.Text = "Upload success.";
+        }
+    }
+}
